771e4ee8f4ab77b5a30e01f463e8b3abcf5c5aa3
Srdjan Krstic <krledmno1@gmail.com>
2015-03-27 13:49:37 +0100

    sync

--- CodeExamples/Introduction/Bike/1/Client.java
+++
@@ -1,9 +0,0 @@
-public class Client {
-	
-	public static void main(String[] args)
-    {
-        Bike bike1; // defines a new object of class Bike
-        bike1 = new Bike(); // instantiate  the object
-        Bike bike2 = new Bike(); // defines and instantiate a new object of class Bike
-     }
-}
